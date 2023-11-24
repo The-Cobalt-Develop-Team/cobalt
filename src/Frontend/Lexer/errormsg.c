0af429ce0ceba37d9e5e8e874df4b6e99af988e9
AndyShen2006 <andyshen2006@foxmail.com>
2023-11-24 18:23:48 +0000

    Move some old codes(Especially Lexer) to this project.

---
+++ src/Frontend/Lexer/errormsg.c
@@ -0,0 +1,36 @@
+/*
+    Error message implementation of the Cobalt Compiler
+    Copyright (C) 2023  Andy Shen
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+#include "errormsg.h"
+
+int EM_tokpos = 0;
+
+#define TEST 1
+
+/* The code below is only for TEST, DO NOT COMPILE IT INTO RELEASE VERSION */
+#if TEST
+
+void LOG_ERRORMSG(int pos, char* s)
+{
+    printf("Error:%d,%s", pos, s);
+}
+
+#else
+
+extern void LOG_ERRORMSG();
+
+#endif
---
+++ src/Frontend/Lexer/util.c
@@ -0,0 +1,13 @@
+#include "util.h"
+
+void *checked_malloc(int len) {
+  void *p = malloc(len);
+  assert(p);
+  return p;
+}
+
+C_string C_String(char *s) {
+  C_string p = checked_malloc(strlen(s) + 1);
+  strcpy(p, s);
+  return p;
+}
